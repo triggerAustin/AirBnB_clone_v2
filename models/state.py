866809ab3481847890bc26e489c3cc6dadd05bd2
Void <trigger_austin00@proton.me>
2024-06-27 00:54:30 +0300

    task 6 state

--- models/state.py
+++ models/state.py
@@ -9,3 +9,17 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """ Getter attribute for cities that returns list of City instances
+                with state_id equals to the current State.id """
+            from models import storage
+            city_list = []
+            for city in storage.all('City').values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
