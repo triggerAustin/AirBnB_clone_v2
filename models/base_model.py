230dfdde808093e4515bf590cc888f13d279b2c9
Void <trigger_austin00@proton.me>
2024-06-27 00:45:23 +0300

    task 6 delete fn

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
-
+import models
 
 Base = declarative_base()
 class BaseModel:
