dbb97d3ae86dfde479a39e9c74ab8d98ab7a8d87
penality <trigger_austin00@proton.me>
2024-07-28 23:31:03 +0300

    tasks 0-6: capitalize d c in task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
     """returns c + text"""
-    return 'C' + text.replace('_', ' ')
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
